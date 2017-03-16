955023e0d1000d2ee2f655a5c2cefba7087223ca
Matthew Stevens <MStevens0042@gmail.com>
2017-03-16 01:02:34 -0700

    changes

--- StockWatcher.py
+++ StockWatcher.py
@@ -9,27 +9,38 @@ google = Share("GOOG")
 apple = Share("AAPL")
 ibm = Share("IBM")
 microsoft = Share("MSFT")
-intel = Share("INTC")
 amd = Share("AMD")
 
-stocks = [amazon, google, apple, ibm, microsoft, intel, amd]
-stocksymbols = ["AMZN", "GOOG", "AAPL", "IBM", "MSFT", "INTC", "AMD"]
+stocks = [amazon, google, apple, ibm, microsoft, amd]
+stocksymbols = ["AMZN", "GOOG", "AAPL", "IBM", "MSFT", "AMD"]
 counter = 1
 
+es = Elasticsearch(
+                ['http://search-cs499assignment4-2-6rirx5boxmskqfjrhmfdkodj2q.us-west-2.es.amazonaws.com:80'])
 
 def main(counter):
     for i in range(stocks.__len__()):
-        es = Elasticsearch(['http://search-cs499assignment4-1-jmjogfowz5kwm4lwi5zsoo2afi.us-west-2.es.amazonaws.com:80'])
-        es.index(index='stockprices', doc_type='prices', id=counter, body={
-            'symbol': stocksymbols[i],
-            'name': stocks[i].get_name(),
-            'price': float(stocks[i].get_price()),
-            'change': float(stocks[i].get_change()),
-            'tradetime': stocks[i].get_trade_datetime(),
-            'retrievetime': datetime.now()
-        })
-        counter += 1
-        stocks[i].refresh()
+        try:
+            es.index(index='stockprices', doc_type='prices', id=counter, body={
+                'symbol': stocksymbols[i],
+                'name': stocks[i].get_name(),
+                'price': float(stocks[i].get_price()),
+                'change': float(stocks[i].get_change()),
+                'tradetime': stocks[i].get_trade_datetime(),
+                'retrievetime': datetime.now()
+            })
+            stocks[i].refresh()
+            counter += 1
+            print({
+                'symbol': stocksymbols[i],
+                'name': stocks[i].get_name(),
+                'price': float(stocks[i].get_price()),
+                'change': float(stocks[i].get_change()),
+                'tradetime': stocks[i].get_trade_datetime(),
+                'retrievetime': datetime.now()
+            })
+        except TypeError:
+            print("Type error!")
     time.sleep(30)
     main(counter)
 
